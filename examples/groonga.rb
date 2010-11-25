64ae6186177370b7f7880297a06eedab4c736112
Ryo Onodera <ryoqun@clear-code.com>
2010-11-26 01:56:50 +0900

    first commit

---
+++ examples/groonga.rb
@@ -0,0 +1,301 @@
+# The author disclaims copyright to this source code.
+
+require 'fileutils'
+
+require 'stress-test/stress-test'
+require 'groonga'
+
+module Kernel
+  #def inspect
+  #  ""
+  #end
+end
+
+def time_stamp
+  Time.now.strftime("%Yy%mm%dd%Hh%Mm%Ss%Nns")
+end
+
+module GroongaStressTest
+  class Table < StressTest::Resource
+    attr_reader :name
+    attr_reader :columns
+    def initialize(name)
+      @name = name
+      @columns = {}
+      @records = []
+    end
+
+    def create
+      @table = Groonga::Hash.create(:name => @name, :key_type => "ShortText")
+    end
+
+    def define_column(name)
+      column = @table.define_column(name, "ShortText")
+      @columns[name] = column
+
+      column
+    end
+
+    def define_reference_column(referenced_table)
+      name = to_reference_column_name(referenced_table)
+      column = @table.define_column(name, referenced_table.name)
+      @columns[name] = column
+
+      column
+    end
+
+    def change_column_value(record, column, value)
+      column_name = column.name.split(".").last
+      @table[record.key][column_name] = value
+    end
+
+    def to_reference_column_name(table)
+      "column_#{table.name.downcase}"
+    end
+
+    def add_record(key)
+      record = @table.add(key)
+      @records << record
+
+      record
+    end
+
+    def delete_record(record)
+      record = @records.delete(record)
+      raise "bad" if record.nil?
+      @table.delete(record.key)
+
+      record
+    end
+
+    def select
+      @table.select("_key:key_#{time_stamp}")
+    end
+
+    def remove
+      @table.remove
+    end
+
+    def random_record
+      @records.shuffle.first
+    end
+
+    def random_column
+      @columns.values.shuffle.first
+    end
+  end
+
+  class CreateTable < StressTest::Action
+    def arguments
+      ["Table#{time_stamp}"]
+    end
+
+    def create_table(name)
+      @state.create_resource(Table.new(name))
+    end
+
+    def bad_route?(route, flow)
+      flow.profile.is_a?(StressTest::CloseResourceProfile)
+    end
+  end
+
+  class DefineColumn < StressTest::Action
+    def arguments
+      table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+
+      [table, "column_#{time_stamp}"]
+    end
+
+    def define_column(table, name)
+      table.define_column(name)
+    end
+  end
+
+  class DefineReferenceColumn < StressTest::Action
+    def arguments
+      table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+      referenced_table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+      raise StressTest::Error::BadRoute if table.eql?(referenced_table)
+      raise StressTest::Error::BadRoute if table.columns.include?(table.to_reference_column_name(referenced_table))
+
+      [table, referenced_table]
+    end
+
+    def define_reference_column(table, referenced_table)
+      table.define_reference_column(referenced_table)
+    end
+  end
+
+  class ChangeColumnValue < StressTest::Action
+    def arguments
+      table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+      record = table.random_record
+      raise StressTest::Error::BadRoute if record.nil?
+      column = table.random_column
+      raise StressTest::Error::BadRoute if column.nil?
+      #p column.class.name
+      value = "aaa"
+
+      [table, record, column, value]
+    end
+
+    def change_column_value(table, record, column, value)
+      table.change_column_value(record, column, value)
+    end
+  end
+
+  class AddRecord < StressTest::Action
+    def arguments
+      table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+
+      [table, "key_#{time_stamp}"]
+    end
+
+    def add_record(table, key)
+      table.add_record(key)
+    end
+  end
+
+  class DeleteRecord < StressTest::Action
+    def arguments
+      table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+      record = table.random_record
+      raise StressTest::Error::BadRoute if record.nil?
+
+      [table, record]
+    end
+
+    def delete_record(table, record)
+      table.delete_record(record)
+    end
+  end
+
+  class RemoveTable < StressTest::Action
+    def arguments
+      table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+
+      [table]
+    end
+
+    def remove_table(table)
+      @state.remove_resource(table)
+    end
+  end
+
+  class Select < StressTest::Action
+    def arguments
+      table = @state.random_table
+      raise StressTest::Error::BadRoute if table.nil?
+
+      [table]
+    end
+
+    def select(table)
+      table.select
+    end
+  end
+
+  module StateInitializer
+    def on_initialize
+      _database_path = database_path
+      puts _database_path
+      @database = Groonga::Database.create(:path => _database_path)
+    end
+
+    def database_path
+      ENV["DATABASE_PATH"] || choose_database_path
+    end
+
+    def choose_database_path
+      random_directory = "/tmp/groonga-steress-test-databases/#{time_stamp}/"
+      FileUtils.mkdir_p(random_directory)
+      database_path = random_directory + "db"
+      if File.exists?(database_path)
+        raise "exists!"
+      end
+      database_path
+    end
+  end
+
+  class State < StressTest::State
+    include StateInitializer
+
+    define_action :create_table, CreateTable
+    define_action :remove_table, RemoveTable
+    define_action :define_column, DefineColumn
+    define_action :define_reference_column, DefineReferenceColumn
+    define_action :change_column_value, ChangeColumnValue
+    define_action :add_record, AddRecord
+    define_action :delete_record, DeleteRecord
+    define_action :select, Select
+
+    def random_table
+      resource_set.resources.shuffle.first
+    end
+  end
+
+  class StateGroup < StressTest::StateGroup
+    def on_initialize
+    end
+
+    def on_create_state(state)
+    end
+
+    class << self
+      def state_class
+        State
+      end
+    end
+  end
+
+  class Flow < StressTest::Flow
+    initial_action :create_table
+
+    route :create_table, :to => :default, :from => :default
+    route :remove_table, :to => :default, :from => :default
+    route :define_column, :to => :default, :from => :default
+    route :define_reference_column, :to => :default, :from => :default
+    route :change_column_value, :to => :default, :from => :default
+    route :add_record, :to => :default, :from => :default
+    route :delete_record, :to => :default, :from => :default
+    route :select, :to => :default, :from => :default
+
+    class << self
+      def create_model_state
+        State.new
+      end
+    end
+  end
+end
+
+=begin
+Thread.new do
+  loop do
+    puts "GC.start"
+    GC.start
+    seconds = Random.new.rand(0.1..1)
+    puts "sleep #{seconds}"
+    sleep seconds
+  end
+end
+=end
+
+state_group = GroongaStressTest::StateGroup.new
+
+runner = StressTest::Runner.new(GroongaStressTest::Flow.new,
+                                state_group.create_state,
+                                :run_count => 100000,
+                                :sleep_second => 0)
+begin
+  runner.run
+#rescue
+end
+
+puts "exiting from ruby...."
---
+++ lib/stress-test/stress-test.rb
@@ -0,0 +1,297 @@
+# The author disclaims copyright to this source code.
+
+require 'pp'
+
+module StressTest
+  class Error < StandardError
+    class BadRoute < Error
+    end
+  end
+
+  class ResourceSet
+    attr_reader :resources
+    def initialize
+      @resources = []
+    end
+
+    def add(resource)
+      @resources << resource
+    end
+
+    def delete(resource)
+      raise "bad" if (resource = @resources.delete(resource)).nil?
+    end
+
+    def empty?
+      @resources.empty?
+    end
+  end
+
+  class Resource
+    attr_writer :state
+
+    def initialize
+    end
+  end
+
+  class Action
+    def initialize(state)
+      @state = state
+    end
+
+    def arguments
+      raise "implement"
+    end
+
+    def block
+      nil
+    end
+
+    def bad_route?(route, flow)
+      false
+    end
+  end
+
+  class StateGroup
+    def initialize
+      @states = []
+      @resource_set = ResourceSet.new
+      on_initialize
+    end
+
+    def create_state
+      state = self.class.state_class.new(@resource_set)
+      state.state_group = self
+      on_create_state(state)
+      @states << state
+      state
+    end
+
+    def on_initialize
+    end
+
+    def on_create_state(state)
+    end
+  end
+
+  class State
+    attr_reader :actions
+    attr_writer :state_group
+    attr_reader :resource_set
+    def initialize(resource_set=ResourceSet.new)
+      @is_initialized_state = false
+      @resource_set = resource_set
+      @actions = {}
+      @@actions.each do |name, action|
+        @actions[name] = action.new(self)
+      end
+      on_initialize
+    end
+
+    def action(name)
+      @actions[name]
+    end
+
+    def initialized_state?
+      @is_initialized_state
+    end
+
+    def is_initialized_state
+      @is_initialized_state = true
+    end
+
+    attr_accessor :current_state
+
+    def create_resource(resource)
+      raise "bad: #{resource.class}" unless resource.is_a?(Resource)
+      resource.state = self
+      resource.create
+      @resource_set.add(resource)
+      resource
+    end
+
+    def remove_resource(resource)
+      raise "bad: #{resource.class}" unless resource.is_a?(Resource)
+      @resource_set.delete(resource)
+      resource.remove
+    end
+
+    def no_resource_opened?
+      @resource_set.empty?
+    end
+
+    def do_action(action, *arguments)
+      puts "#{time_stamp}: #{action}"
+      @actions[action].__send__(action, *arguments)
+    end
+
+    class << self
+      def define_action(name, action)
+        @@actions ||= {}
+        @@actions[name] = action
+      end
+    end
+  end
+
+  class PseudoState < State # written-by-ruby in-memory database for stress-testing real database
+  end
+
+  class Route
+    def initialize(options)
+      @options = options
+    end
+
+    def transit
+    end
+
+    def to
+      @options[:to]
+    end
+
+    def from
+      @options[:from]
+    end
+  end
+
+  class Transition
+    attr_accessor :action
+    attr_accessor :arguments
+    attr_accessor :route
+  end
+
+  class Profile
+  end
+
+  class NormalProfile < Profile
+  end
+
+  class CloseResourceProfile < Profile
+  end
+
+  class Flow
+    attr_reader :profile
+    def initialize
+      @profile = NormalProfile.new
+    end
+
+    def begin_termination
+      @profile = CloseResourceProfile.new
+    end
+
+    def next_transition(state)
+      if not state.initialized_state?
+        initial_transition(state)
+      else
+        random_transition_from_current_state(state)
+      end
+    end
+
+    def initial_transition(state)
+      transition = Transition.new
+      transition.action = @@initial_action
+      transition.arguments = state.action(transition.action).arguments
+      transition.route = @@routes[@@initial_action]
+      transition
+    end
+
+    def transit_state(state, transition)
+      ensure_mark_state_as_initialized(state)
+
+      state.do_action(transition.action, *transition.arguments)
+      state.current_state = transition.route.to
+    end
+
+    def ensure_mark_state_as_initialized(state)
+      if not state.initialized_state?
+        state.is_initialized_state
+      end
+    end
+
+    private
+    def random_transition_from_current_state(state)
+      action, arguments, route = select_random_route(state)
+
+      transition = Transition.new
+      transition.action = action
+      transition.arguments = arguments
+      transition.route = route
+      transition
+    end
+
+    def select_random_route(state)
+      begin
+        try_select_random_route(state)
+      rescue Error::BadRoute
+        retry
+      end
+    end
+
+    def try_select_random_route(state)
+      action, route = randomly_select_route(state)
+      if state.action(action).bad_route?(route, self)
+        raise Error::BadRoute
+      end
+      arguments = state.action(action).arguments
+
+      [action, arguments, route]
+    end
+
+    def randomly_select_route(state)
+      routes = @@routes.select do |action, route|
+        route.from == state.current_state
+      end
+      routes.to_a.shuffle.first
+   end
+
+    class << self
+      def initial_action(action)
+        @@initial_action = action
+      end
+
+      def route(action, options)
+        @@routes ||= {}
+        @@routes[action] = Route.new(options)
+      end
+    end
+  end
+
+  class Runner
+    def initialize(flow, state, options={})
+      @flow = flow
+      @state = state
+      @options = options
+    end
+
+    DEFAULT_RUN_COUNT = 10
+    def run_count
+      @options[:run_count] || DEFAULT_RUN_COUNT
+    end
+
+    DEFAULT_SLEEP_SECOND = 1
+    def sleep_second
+      @options[:sleep_second] || DEFAULT_SLEEP_SECOND
+    end
+
+    def run
+      run_count.times do
+        run_once
+      end
+
+      puts "begin_termination"
+      @flow.begin_termination
+
+      until @state.no_resource_opened?
+        run_once
+      end
+    end
+
+    def run_once
+      transition = @flow.next_transition(@state) # @flow can have ideal state/ model state/expected state, Virtual State(only cares about life time of Resources) # maybe this is bad??
+      #puts transition.action
+      #puts transition.inspect
+      #puts @state.resources.inspect
+      @flow.transit_state(@state, transition)
+      sleep sleep_second unless sleep_second.zero?
+    end
+  end
+end
